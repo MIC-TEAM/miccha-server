ad5c50f5dc9a70680d0cce6605ed3b46a708d651
youmustgo <iluvtrollhd@gmail.com>
2021-05-26 21:58:54 +0900

    get list of movie collection by offset

--- src/main/java/com/miccha/server/movie/MovieService.java
+++ src/main/java/com/miccha/server/movie/MovieService.java
@@ -1,10 +1,28 @@
 package com.miccha.server.movie;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.miccha.server.config.Config;
+import com.miccha.server.movie.model.MovieCollection;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@AllArgsConstructor
 @Service
 public class MovieService {
-    @Autowired
-    private MovieRepository repository;
+    private Config config;
+    private MovieRepository movieRepository;
+    private TagRepository tagRepository;
+
+    public Mono<List<MovieCollection>> getPage(int pageOffset) {
+        List<MovieCollection> page = new CopyOnWriteArrayList<>();
+        return tagRepository.getWithBounds(pageOffset * config.getPageSize(), config.getPageSize())
+                            .flatMap(tag -> movieRepository.getByTagId(tag.getId())
+                                                           .collectList()
+                                                           .single()
+                                                           .doOnSuccess(movies -> page.add(new MovieCollection(tag.getValue(), movies))))
+                            .then(Mono.just(page));
+    }
 }
