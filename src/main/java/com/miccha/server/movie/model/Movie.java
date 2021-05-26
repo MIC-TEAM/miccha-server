8518deeaff7b4b6e3a8e3ee5f6fa42d9ed53e14f
youmustgo <iluvtrollhd@gmail.com>
2021-05-26 21:56:49 +0900

    add object mapper to movie model

---
+++ src/main/java/com/miccha/server/movie/model/Movie.java
@@ -0,0 +1,32 @@
+package com.miccha.server.movie.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@Table
+public class Movie {
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private String rating;
+    private Integer duration;
+    private String thumbnail;
+
+    public static Movie of(Map<String, Object> columnMap) {
+        Movie movie = new Movie();
+        movie.setId((Long) columnMap.get("id"));
+        movie.setTitle((String) columnMap.get("title"));
+        movie.setDescription((String) columnMap.get("description"));
+        movie.setRating((String) columnMap.get("rating"));
+        movie.setDuration((Integer) columnMap.get("duration"));
+        movie.setTitle((String) columnMap.get("thumbnail"));
+        return movie;
+    }
+}
