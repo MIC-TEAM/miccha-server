1a7dafd88f18ba666ea1c13729db56a0e0083fc9
youmustgo <iluvtrollhd@gmail.com>
2021-05-28 21:39:52 +0900

    validate email address

--- src/main/java/com/miccha/server/user/UserService.java
+++ src/main/java/com/miccha/server/user/UserService.java
@@ -1,8 +1,10 @@
 package com.miccha.server.user;
 
 import com.miccha.server.exception.DuplicateEmailException;
+import com.miccha.server.exception.InvalidEmailException;
 import com.miccha.server.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,15 +15,21 @@ public class UserService {
     private final UserRepository userRepository;
 
     public Mono<Void> signUp(@NonNull User user) {
-        return userRepository.existsByEmail(user.getEmail())
-                             .single()
-                             .doOnSuccess(exists -> {
-                                 if (exists) {
-                                     throw new DuplicateEmailException();
-                                 }
-                             })
-                             .then(userRepository.save(user))
-                             .single()
-                             .then(Mono.empty());
+        return Mono.just(user)
+                   .doOnNext(userValue -> {
+                       if (EmailValidator.getInstance().isValid(userValue.getEmail()) == false) {
+                           throw new InvalidEmailException();
+                       }
+                   })
+                   .flatMap(userValue -> userRepository.existsByEmail(userValue.getEmail()))
+                   .single()
+                   .doOnSuccess(exists -> {
+                       if (exists) {
+                           throw new DuplicateEmailException();
+                       }
+                   })
+                   .then(userRepository.save(user))
+                   .single()
+                   .then(Mono.empty());
     }
 }
